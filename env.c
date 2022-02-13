9eb410bd49b909e79ebeb58db381f936f58af567
elijahal18 <eal510@nyu.edu>
2022-02-13 13:13:21 -0500

    adding

--- env.c
+++ env.c
@@ -8,21 +8,46 @@
 #include <string.h>
 #include <stdlib.h>
 
-void printenv(char** environ);
+extern const char** environ;
+
+void printenv(char** env);
 
 int main(int argc, char* argv[]) {
     //variables
     char **myenv;
-    int iflag;
+    int iflag=0;
+    int index=0;
     if (argc == 1){
         printenv();
         return 0;
     }
+    else if (!strcmp(argv[1], "-i")){
+        iflag = 1;
+        for (int i = 2; i < argc; i++) {
+            if (strchr(argv[i], '=') == NULL) {
+                index=i;
+            }
+        }
+        if (index == 0){
+            index=argc;
+        }
+    }
+    else{// no -i
+        iflag=0
+        for (int i = 1; i < argc; i++) {
+            if (strchr(argv[i], '=') == NULL) { 
+                index=i;
+            }
+        }
+        if (index == 0){
+            index=argc;
+        }
+    }
     
 }
 
-void printenv(char** environ){
-    for (int i = 0; environ[i] != NULL; i++) {
-        printf("%s\n", environ[i]);
+void printenv(char** env){
+    for (int i = 0; env[i] != NULL; i++) {
+        printf("%s\n", env[i]);
     }
 }
